7b7c60aed003f1ff275069b9a5357c01c630dda2
Antoine Neff <9216777+antoineneff@users.noreply.github.com>
2021-08-30 19:24:31 +0200

    feat: pretty print data in a table

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use clap::{App, Arg};
 use serde::Deserialize;
 use image::GenericImageView;
+use term_table::{Table, TableStyle};
+use term_table::row::Row;
+use term_table::table_cell::{TableCell, Alignment};
 
 const UPPER_HALF_BLOCK: &str = "\u{2580}";
 const LOWER_HALF_BLOCK: &str = "\u{2584}";
@@ -35,14 +38,24 @@ fn main() -> Result<(), reqwest::Error> {
     let uri = format!("https://pokeapi.co/api/v2/pokemon/{}", search);
     let pokemon: Pokemon = reqwest::blocking::get(uri)?.json()?;
 
-    println!("id: {}", pokemon.id);
-    println!("name: {}", pokemon.name);
-    println!("weight: {:.1}kg", pokemon.weight as f32 / 10.0);
-    println!("height: {:.1}m", pokemon.height as f32 / 10.0);
+    let mut table = Table::new();
+    table.max_column_width = 96;
+    table.style = TableStyle::extended();
+
+    table.add_row(Row::new(vec![
+        TableCell::new_with_alignment(pokemon.name.to_uppercase(), 3, Alignment::Center),
+    ]));
+    table.add_row(Row::new(vec![
+        TableCell::new_with_alignment(format!("# {}", pokemon.id), 1, Alignment::Center),
+        TableCell::new_with_alignment(format!("{:.1}kg", pokemon.weight as f32 / 10.0), 1, Alignment::Center),
+        TableCell::new_with_alignment(format!("{:.2}m", pokemon.height as f32 / 10.0), 1, Alignment::Center)
+    ]));
 
     let img_bytes = reqwest::blocking::get(pokemon.sprites.front_default)?.bytes()?;
     let img = image::load_from_memory(&img_bytes).unwrap();
     let (width, height) = img.dimensions();
+
+    let mut pokemon_str = String::new();
 
     for y in (0..height).step_by(2)  {
         for x in 0..width {
@@ -53,26 +66,32 @@ fn main() -> Result<(), reqwest::Error> {
 
             // Both transparent
             if alpha == 0 && alpha2 == 0 {
-                print!(" ")
+                pokemon_str.push_str(" ");
             }
             // Top half transparent, set colored lower half block
             else if alpha == 0 {
                 let prefix = format!("\x1B[38;2;{};{};{}m", r2, g2, b2);
-                print!("{}{}{}", prefix, LOWER_HALF_BLOCK, POSTFIX);
+                pokemon_str.push_str(&format!("{}{}{}", prefix, LOWER_HALF_BLOCK, POSTFIX));
             }
             // Bottom half transparent, set colored top half block
             else if alpha2 == 0 {
                 let prefix = format!("\x1B[38;2;{};{};{}m", r, g, b);
-                print!("{}{}{}", prefix, UPPER_HALF_BLOCK, POSTFIX);
+                pokemon_str.push_str(&format!("{}{}{}", prefix, UPPER_HALF_BLOCK, POSTFIX));
             }
             // Both blocks are colored, set colored top half block with foreground color
             else {
                 let prefix = format!("\x1B[38;2;{};{};{}m\x1B[48;2;{};{};{}m", r, g, b, r2, g2, b2);
-                print!("{}{}{}", prefix, UPPER_HALF_BLOCK, POSTFIX);
+                pokemon_str.push_str(&format!("{}{}{}", prefix, UPPER_HALF_BLOCK, POSTFIX));
             }
         }
-        println!()
+        pokemon_str.push_str("\n");
     }
+
+    table.add_row(Row::new(vec![
+        TableCell::new_with_alignment(pokemon_str, 3, Alignment::Center)
+    ]));
+
+    println!("{}", table.render());
 
     Ok(())
 }
